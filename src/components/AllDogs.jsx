d1a527763074159154cb58e9801490470fe7f70b
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2023-10-02 12:49:51 +0200

    dogdetail works now

--- src/components/AllDogs.jsx
+++ src/components/AllDogs.jsx
@@ -3,175 +3,230 @@ import './AllDogs.css';
 
 const AllDogs = (props) => {
 
-    const dogArray = props.dogs
-    .filter(dog => dog.present) 
+
+
+  const dogArray = props.dogs
+    .filter(dog => dog.present)
     .concat(props.dogs.filter(dog => !dog.present));
 
-    //console.log(dogArray);
+  //console.log(dogArray);
 
-    useEffect(() => {
+  useEffect(() => {
 
-        createDog(dogArray);
-        }, []);
-
-
-   // const overlay =document.getElementById('overlay');
-    //const overlayImg = document.querySelector('#overlay img');
-    //const overlayTitle = document.querySelector('#overlay h2');
+    createDog(dogArray);
+  }, []);
 
 
+  const createDog = (dogs) => {
 
-    const createDog = (dogs) =>{
+    const dogsSection = document.getElementById('allDogs');
+    const overlay = document.getElementById('overlay');
 
-        const dogsSection = document.getElementById('allDogs');
-        const overlay =document.getElementById('overlay'); 
+    dogs.forEach(dog => {
 
-        dogs.forEach(dog => {
+      const dogElement = createDogElements(dog);
 
-            const dogElement = createDogElements(dog);
-           
-          
-            dogsSection.appendChild(dogElement);
 
-            dogElement.addEventListener('click', () =>{
-              
-                overlay.appendChild(dogElement)
-                //dogDescription.className = "shown";
-                console.log('syntax problem')
-                overlay.classList.toggle('show')
-                overlay.addEventListener('click', () => {
-                  overlay.removeChild(dogElement);
-                  overlay.classList.remove('show');
-              });
-             })
+      dogsSection.appendChild(dogElement);
+
+      dogElement.addEventListener('click', () => {
+        const detailedDog = document.createElement('div');
+        detailedDog.className = 'dogDetail';
   
+        const detailHeading = document.createElement('h2');
+        detailHeading.innerHTML = `${dog.name} ${"\u{1F43E}"}`;
+        detailedDog.appendChild(detailHeading);
+
         
-        });
-      
-      
-       
+    const dogPicContainer = document.createElement('div');
+    if (dog.present) {
+      dogPicContainer.className = 'dogPicIn';
+    }
+    else {
+      dogPicContainer.className = 'dogPicOut';
+    }
+    const dogPic = document.createElement('img')
+    dogPic.src = dog.img;
+    dogPic.alt = dog.name;
+    dogPic.onload = function () {
+      if (dogPic.height > dogPic.width) {
+        dogPic.className = 'dogWidth'
       }
 
-  
-
-
-      const dogDetail = (dogElement) =>{
-
-        overlay.appendChild(dogElement)
-        //overlayImg = img;
-        overlay.classList.toggle('show')
-    
-
-
+      else {
+        dogPic.className = 'dogHeight'
       }
-
-      const createDogElements = (dog) =>{
-
-        const dogElement = document.createElement('div');
-        dogElement.className ='dog';
-
-        const dogHeading = document.createElement('h2');
-        dogHeading.innerHTML = `${dog.name} ${"\u{1F43E}"}`;
-
-     const dogPicContainer = document.createElement('div');
-       if (dog.present){
-       dogPicContainer.className='dogPicIn';}
-       else{
-        dogPicContainer.className='dogPicOut';}
-       
-       
-       
-
-        const dogPic = document.createElement('img')
-        dogPic.src = dog.img;
-        dogPic.alt = dog.name;
-
-        
-
-        dogPic.onload = function(){
-        if (dogPic.height > dogPic.width){
-            dogPic.className = 'dogWidth'
-        }
-        
-        else{
-            dogPic.className = 'dogHeight'
-        }
     };
-        dogPicContainer.appendChild(dogPic)
-        const dogDescription = document.createElement('div');
-        const dogSex = document.createElement('p')
-       
+    dogPicContainer.appendChild(dogPic)
+    const dogDescription = document.createElement('div');
+    const dogSex = document.createElement('p')
 
-        if(dog.sex === "female"){
-            
-        dogSex.innerText = `Tik`
-        ;}
-        else{
-            //dogSex.innerText = `Kön: hane ${dog.sex}`;}
-      
-            dogSex.innerText = `Hane`;}
-      
+
+    if (dog.sex === "female") {
+
+      dogSex.innerText = `Good Girl!`
         ;
+    }
+    else {
+      dogSex.innerText = `Good boy`;
+    }
 
-        dogDescription.appendChild(dogSex);
-        
-        const chipNumber = document.createElement('p')
-        chipNumber.innerText =`Chipnumret: ${ dog.chipNumber}`; 
-        dogDescription.appendChild(chipNumber);
-      
-        const ownerInformation = document.createElement('div')
-        const ownerHeading = document.createElement("h2");
-        ownerHeading.innerText = "Ägare"
-        ownerInformation.appendChild(ownerHeading);
-        const ownerName = document.createElement('p')
-        ownerName.innerText = `${dog.owner.name} ${dog.owner.lastName}`; 
-        ownerInformation.appendChild(ownerName);
-      
-      
-         const ownerPhone = document.createElement('p')
-        ownerPhone.innerText = `Tel.: ${dog.owner.phoneNumber}`; 
-        ownerInformation.appendChild(ownerPhone);
-        
-        //dogDescription.className = 'hidden';
-      
-      dogDescription.appendChild(ownerInformation)
-       
-       dogElement.appendChild(dogPicContainer);
-      
-        dogElement.appendChild(dogHeading);
+    ;
 
-        dogElement.appendChild(dogDescription);
+    dogDescription.appendChild(dogSex);
 
-   
-    dogDescription.className = "hidden"
-       dogElement.addEventListener('click', () => {
+    const chipNumber = document.createElement('p')
+    chipNumber.innerText = `Chipnumret: ${dog.chipNumber}`;
+    dogDescription.appendChild(chipNumber);
+
+    const ownerInformation = document.createElement('div')
+    const ownerHeading = document.createElement("h2");
+    ownerHeading.innerText = "Ägare"
+    ownerInformation.appendChild(ownerHeading);
+    const ownerName = document.createElement('p')
+    ownerName.innerText = `${dog.owner.name} ${dog.owner.lastName}`;
+    ownerInformation.appendChild(ownerName);
 
 
-           dogDescription.classList = "show";
+    const ownerPhone = document.createElement('p')
+    ownerPhone.innerText = `Tel.: ${dog.owner.phoneNumber}`;
+    ownerInformation.appendChild(ownerPhone);
+
+    //dogDescription.className = 'hidden';
+
+    dogDescription.appendChild(ownerInformation)
+
+    detailedDog.appendChild(dogPicContainer);
+
+    
+    detailedDog.appendChild(dogDescription);
+
+        overlay.appendChild(detailedDog);
   
-              console.log("HEj")
-             });
-     
-        return dogElement;
+        overlay.classList.toggle('show');
+  
+        overlay.addEventListener('click', () => {
+          overlay.innerHTML = ''; 
+          overlay.classList.remove('show');
+        });
+      });
+    });
+  };
 
 
+  
+
+  const createDogElements = (dog) => {
+
+    const dogElement = document.createElement('div');
+    dogElement.className = 'dog';
+
+    const dogHeading = document.createElement('h2');
+    dogHeading.innerHTML = `${dog.name} ${"\u{1F43E}"}`;
+
+    const dogPicContainer = document.createElement('div');
+    if (dog.present) {
+      dogPicContainer.className = 'dogPicIn';
+    }
+    else {
+      dogPicContainer.className = 'dogPicOut';
+    }
+
+
+
+
+    const dogPic = document.createElement('img')
+    dogPic.src = dog.img;
+    dogPic.alt = dog.name;
+
+
+
+    dogPic.onload = function () {
+      if (dogPic.height > dogPic.width) {
+        dogPic.className = 'dogWidth'
       }
 
+      else {
+        dogPic.className = 'dogHeight'
+      }
+    };
+    dogPicContainer.appendChild(dogPic)
+    /*const dogDescription = document.createElement('div');
+    const dogSex = document.createElement('p')
 
-console.log({dogArray});
-    return (
-        <section id="allDogs">
-           <div className="h3Dog"> <h3> Våra hundar</h3></div>
-           
-           
-           <div id= "overlay">
-           
 
-           </div>
-        </section>
-        
+    if (dog.sex === "female") {
 
-    )
+      dogSex.innerText = `Tik`
+        ;
+    }
+    else {
+      //dogSex.innerText = `Kön: hane ${dog.sex}`;}
+
+      dogSex.innerText = `Hane`;
+    }
+
+    ;
+
+    dogDescription.appendChild(dogSex);
+
+    const chipNumber = document.createElement('p')
+    chipNumber.innerText = `Chipnumret: ${dog.chipNumber}`;
+    dogDescription.appendChild(chipNumber);
+
+    const ownerInformation = document.createElement('div')
+    const ownerHeading = document.createElement("h2");
+    ownerHeading.innerText = "Ägare"
+    ownerInformation.appendChild(ownerHeading);
+    const ownerName = document.createElement('p')
+    ownerName.innerText = `${dog.owner.name} ${dog.owner.lastName}`;
+    ownerInformation.appendChild(ownerName);
+
+
+    const ownerPhone = document.createElement('p')
+    ownerPhone.innerText = `Tel.: ${dog.owner.phoneNumber}`;
+    ownerInformation.appendChild(ownerPhone);
+
+    //dogDescription.className = 'hidden';
+
+    dogDescription.appendChild(ownerInformation)*/
+
+    dogElement.appendChild(dogPicContainer);
+
+    dogElement.appendChild(dogHeading);
+
+   // dogElement.appendChild(dogDescription);
+
+
+  /*  dogDescription.className = "hidden"
+    dogElement.addEventListener('click', () => {
+
+
+      dogDescription.classList = "show";
+
+      
+    });*/
+
+    return dogElement;
+
+
+  }
+
+
+  console.log({ dogArray });
+  return (
+    <section id="allDogs">
+      <div className="h3Dog"> <h3> Våra hundar</h3></div>
+
+
+      <div id="overlay">
+
+
+      </div>
+    </section>
+
+
+  )
 }
 
 export default AllDogs;
